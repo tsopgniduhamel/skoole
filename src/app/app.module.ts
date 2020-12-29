4116a35a25aec67d4949f86cbf0860ec767354d2
tsopgniduhamel <tsopgniduhamel@gmail.com>
2020-12-29 02:43:22 +0100

    creation du component main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, MainComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
