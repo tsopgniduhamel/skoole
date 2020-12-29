abfaf3528d9605b61768542a118c9d56cd3bf6af
tsopgniduhamel <tsopgniduhamel@gmail.com>
2020-12-29 01:43:45 +0100

    creating the header component that will be responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
