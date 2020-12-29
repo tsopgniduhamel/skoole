ec36f92ee3b737ed1fa0ca29771e3fb41a0d4d66
tsopgniduhamel <tsopgniduhamel@gmail.com>
2020-12-29 01:49:11 +0100

    testing the responsiveness display of the header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @Input() deviceXs: boolean;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
