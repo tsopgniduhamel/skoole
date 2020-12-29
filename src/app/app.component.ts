7ba4d1fe1d42b3faffcefa836932522e80e70320
tsopgniduhamel <tsopgniduhamel@gmail.com>
2020-12-29 01:42:32 +0100

    adding the flexlayout module and displaying the size of medias in the console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MediaChange, MediaObserver } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'skoole';
+
+  mediaSub: Subscription;
+  deviceXs: boolean;
+
+  constructor(private mediaObserver: MediaObserver) {}
+  ngOnInit() {
+    this.mediaSub = this.mediaObserver.media$.subscribe(
+      (result: MediaChange) => {
+        console.log(result.mqAlias);
+        this.deviceXs = result.mqAlias === 'xs' ? true : false;
+      }
+    );
+  }
+  ngOnDestroy() {
+    this.mediaSub.unsubscribe();
+  }
 }
