e6b89245bfab98e1e05d453db80b92b48cc268b3
tsopgniduhamel <tsopgniduhamel@gmail.com>
2021-01-05 13:45:29 +0100

    tout est bon jusqu'ici; je dois maintenant refaire le navbar en utilisant bootstratp

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { CoursService } from './../services/cours.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  courses: any;
 
-  constructor() { }
+  constructor(private courseService: CoursService) {}
 
   ngOnInit(): void {
+    this.AllCourses();
   }
 
+  AllCourses() {
+    this.courseService.getAllCourses().subscribe((courses) => {
+      this.courses = courses;
+      console.log(this.courses);
+    });
+  }
 }
