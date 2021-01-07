7b518d708f540ed6d5a0710dc3c65cc6c4f70c8c
tsopgniduhamel <tsopgniduhamel@gmail.com>
2021-01-07 17:31:17 +0100

    nouvelle modification du fichier server.js car l'application ne donne toujours pas sur le navigateur

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/skoole-frontend'));
 
-app.get('/*', function(req,res) {
+// app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/skoole-frontend/index.html'));
+// res.sendFile(path.join(__dirname+'/dist/skoole-frontend/index.html'));
+// });
+
+app.get('*', (req, res) => {
+  res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 // Start the app by listening on the default Heroku port
